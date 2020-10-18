3ef5947752cd40a24ee6239b184a4ac9c1caf1bc
vit <anhxtanhwp@gmail.com>
2020-10-18 22:32:58 +0700

    update

--- app/src/main/java/com/vit/leepair/MainActivity.kt
+++ app/src/main/java/com/vit/leepair/MainActivity.kt
@@ -18,21 +18,23 @@ class MainActivity : AppCompatActivity() {
         button_direct.setOnClickListener {
             val intent = Intent(Intent.ACTION_VIEW)
             intent.data =
-                Uri.parse("leeplayer://player?type=movie&link=https://www2.himovies.to/api/movie/info/39539&episodeId=2479")
+                Uri.parse("leeplayer://player?type=movie&link=https://www2.himovies.to/api/movie/info/39539&episodeId=2479&start_time=10000")
             startActivity(intent)
         }
 
         button_movie.setOnClickListener {
             val intent = Intent(Intent.ACTION_VIEW)
             intent.data =
-                Uri.parse("leeplayer://player?type=movie&link=https://www2.himovies.to/api/movie/info/6223")
+                Uri.parse("leeplayer://player?type=movie&link=https://www2.himovies.to/api/movie/info/6223&start_time=10000")
             startActivity(intent)
         }
 
         button_download.setOnClickListener {
             val intent = Intent(Intent.ACTION_VIEW)
-            intent.data = Uri.parse("abc://abc/mybox")
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            intent.data = Uri.parse("leeplayer://download?download_url=https://b-g-d-fr-1.betterstream.co/download/7bb7ce66880ba70393efe868138b3ab90f0e3e2e3d7261f178ef4370b81dfe8aa5e5d9cfe8b1678dd4623c32f422418c7973e821ee3f4a1040dc02def12563a4da24c3e4d463d87e3105c31a57b35fc3689998c9625d6288326687713ac25b05387645d287e5022f7479decd536ad26ac7713675a55958e6ffda7e9f29f978738a2569979819924b39f59a63c3c691c46293ddc025b81ee673da78ee991aaecc" +
+                    "&callback_url=https://cnh.tmdbapi.ru/api/mobile/1/download_manager/insert" +
+                    "&subtitle_url=https://thepaciellogroup.github.io/AT-browser-tests/video/subtitles-en.vtt" +
+                    "&lang_name=English Viet")
             startActivity(intent)
         }
     }
